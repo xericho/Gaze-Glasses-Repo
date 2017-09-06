243f19da156711b01ef9e39ae562771a53dbc682
xericho <xeric.hox@gmail.com>
2017-09-06 14:51:51 -0700

    updated main scrips

--- Confusion Matrix/hitscan_update.m
+++ Confusion Matrix/hitscan_update.m
@@ -1,4 +1,4 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This script uses the csv bboxes and finds the RLE for analysis.
 % It also overlay the bbox in the video for vizualization
 % 
@@ -10,72 +10,47 @@
 % Files required: - a video
 %                 - CSV bboxes of the faces/objects from video
 %                 - the gaze position from video
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Load csv files and dilate bboxes to account for error
 % close all
-% clear all
+clear all
 % clc
 
 % path of folder which contains all the files for that video
-addpath('Vid002')
+foldername = 'vid004';
+addpath(foldername)
 
+% frame_import = csvimport('vid002_frame_90_all_frames.csv');
+% frame = str2double(frame_import(:,:));
 frame = ind_import('frame_90_5fps.csv');        % load csv file
-frame = dilate(frame,1);                       % dilate bbox by 25%
+% frame = dilate(frame,10);                       % dilate bbox by 25%
 
+% shark_import = csvimport('vid002_shark_50_all_frames.csv');
+% shark = str2double(shark_import(:,2:5));
 shark = ind_import('shark_30_5fps.csv');
-shark = dilate(shark,25);
+% shark = dilate(shark,25);
 
+% top_import = csvimport('vid002_top_90_all_frames.csv');
+% top = str2double(top_import(:,2:5));
 top = ind_import('top_90_5fps.csv');
-top = dilate(top,10);
+% top = dilate(top,10);
 
 load('face1.mat');
 face1 = squeeze(face1_identified)';
+face1 = face_expand(face1);
 load('face2.mat');
-face2 = squeeze(face2_identified)';
+face2 = squeeze(face2_identified)'; %comment for vid003
+face2 = face_expand(face2);
 load('face3.mat');
 face3 = squeeze(face3_identified)';
+face3 = face_expand(face3);
 
 %% Load video and gaze position
-reader = VideoReader('vid002-60fps.mp4');
+reader = VideoReader(sprintf('%s-60fps.mp4',foldername));
 vid_frame_count = reader.NumberOfFrames;
 
-[new_confidence,pos_x,pos_y] = clean_gaze_position('vid002_gaze_positions.csv', reader);
-
-%%
-% w=VideoWriter('output_vid001.mp4','MPEG-4');
-% w.FrameRate=60;
-% open(w)
-for i=3490:1:size(new_confidence,2)
-    img = read(reader,i);               % read a frame
-    imshow(img)                         % show frame
-    % plot gaze position
-    rectangle('Position',[pos_x(i)-5,pos_y(i)-5,10,10],'FaceColor','[0 0.5 0.5]');
-    % plot bboxes of objects
-    if ~isnan(face1_gt(i,1))
-        rectangle('Position',face1_gt(i,:),'EdgeColor','r','LineWidth',2,'LineStyle','--');
-    end
-    if ~isnan(face2_gt(i,1))
-        rectangle('Position',face2_gt(i,:),'EdgeColor','b','LineWidth',3,'LineStyle','--');
-    end
-    if ~isnan(face3_gt(i,1))
-        rectangle('Position',face3_gt(i,:),'EdgeColor','g','LineWidth',3,'LineStyle','--');
-    end
-    if ~isnan(frame_gt(i,1))
-        rectangle('Position',frame_gt(i,:),'EdgeColor','g','LineWidth',3,'LineStyle','--');
-        end
-    if ~isnan(top_gt(i,1))
-        rectangle('Position',top_gt(i,:),'EdgeColor','g','LineWidth',3,'LineStyle','--');
-        end
-    if ~isnan(shark_gt(i,1))
-        rectangle('Position',shark_gt(i,:),'EdgeColor','g','LineWidth',3,'LineStyle','--');
-    end
-    title(i);
-%     f=getframe;
-%     writeVideo(w,f);
-
-    pause(.000000000000000001)
-end    
+[new_confidence,pos_x,pos_y] = clean_gaze_position(sprintf('%s_gaze_positions.csv',foldername), reader);
 
 %% Expand csv bboxes to have a bbox for every frame
 % The csv bboxes are currently sampled every 5 frames
@@ -85,8 +60,8 @@ frame_sample = zeros(size(frame,1), 4);
 shark_sample = zeros(size(shark,1), 4);
 top_sample   = zeros(size(top,1), 4);
 
-for i=1:size(frame,1)*5-1
-    ref = 1+floor(i/5);
+for i=1:size(frame,1)*5
+    ref = 1+floor((i-1)/5);
 %     if i >= 3502 && i <= 4001
 %         continue 
 %     end
@@ -96,41 +71,44 @@ for i=1:size(frame,1)*5-1
 end
 
 %% Overlay bbox and gaze position onto video
-
-% w=VideoWriter('output.mp4','MPEG-4');
-% w.FrameRate=60;
-% open(w)
-
-for i=1:1:size(frame_sample,1)
-    img = read(reader,i);               % read a frame
-    imshow(img)                         % show frame
-    % plot gaze position
-    rectangle('Position',[pos_x(i)-5,pos_y(i)-5,10,10],'FaceColor','[0 0.5 0.5]');
-    % plot bboxes of objects
-    if ~isnan(frame_sample(i,1))
-        rectangle('Position',frame_sample(i,:),'EdgeColor','r','LineWidth',2,'LineStyle','--');
-    end
-    if ~isnan(shark_sample(i,1))
-        rectangle('Position',shark_sample(i,:),'EdgeColor','b','LineWidth',3,'LineStyle','--');
-    end
-    if ~isnan(top_sample(i,1))
-        rectangle('Position',top_sample(i,:),'EdgeColor','g','LineWidth',3,'LineStyle','--');
-    end
-    title(i);
-    pause
-%     f=getframe;
-%     writeVideo(w,f);
-end    
-%  close(w)   
+% 
+% % w=VideoWriter('output.mp4','MPEG-4');
+% % w.FrameRate=60;
+% % open(w)
+% 
+% for i=7300:1:size(frame_sample,1)
+%     img = read(reader,i);               % read a frame
+%     imshow(img)                         % show frame
+%     % plot gaze position
+%     rectangle('Position',[pos_x(i)-5,pos_y(i)-5,10,10],'FaceColor','[0 0.5 0.5]');
+%     % plot bboxes of objects
+%     if ~isnan(frame_sample(i,1))
+%         rectangle('Position',frame_sample(i,:),'EdgeColor','r','LineWidth',2,'LineStyle','--');
+%     end
+%     if ~isnan(shark_sample(i,1))
+%         rectangle('Position',shark_sample(i,:),'EdgeColor','b','LineWidth',3,'LineStyle','--');
+%     end
+%     if ~isnan(top_sample(i,1))
+%         rectangle('Position',top_sample(i,:),'EdgeColor','g','LineWidth',3,'LineStyle','--');
+%     end
+%     if ~isnan(face1(i,1))
+%         rectangle('Position',face1(i,:),'EdgeColor','g','LineWidth',3,'LineStyle','--');
+%     end
+%     if ~isnan(face1_gt(i,1))
+%         rectangle('Position',face1_gt(i,:),'EdgeColor','g','LineWidth',3);
+%     end
+% 
+%     title(i);
+%     pause(.000000000000000001)
+% %     f=getframe;
+% %     writeVideo(w,f);
+% end    
+% %  close(w)   
 
 %% Check if gaze is in bbox
 % Sometimes one is bigger than the other so use the smaller one to avoid error
-if size(frame_sample,1) > size(new_confidence,2)
-    max_length = size(new_confidence,2);
-else
-    max_length = size(frame_sample,1); 
-end
 
+max_length = min(size(frame_sample,1),size(new_confidence,2));
 conf = .25;
 for i=1:max_length
     ref=i;
@@ -144,9 +122,30 @@ for i=1:max_length
     end
 end
 
+%% Check if gaze is in bbox (for ALL frames)
+% Sometimes one is bigger than the other so use the smaller one to avoid error
+% if size(frame,1) > size(new_confidence,2)
+%     max_length = size(new_confidence,2);
+% else
+%     max_length = size(frame,1); 
+% end
+% 
+% conf = .25;
+% for i=1:max_length
+%     ref=i;
+%     if new_confidence(i) >= conf            % filter out anything less than confidence
+%         frame_binary(i)=(pos_x(i)>=frame(ref,1) && pos_x(i)<=frame(ref,1)+frame(ref,3)) && (pos_y(i)>=frame(ref,2) && pos_y(i)<=frame(ref,2)+frame(ref,4));
+%         shark_binary(i)=(pos_x(i)>=shark(ref,1) && pos_x(i)<=shark(ref,1)+shark(ref,3)) && (pos_y(i)>=shark(ref,2) && pos_y(i)<=shark(ref,2)+shark(ref,4));
+%         top_binary(i)  =(pos_x(i)>=top(ref,1)   && pos_x(i)<=top(ref,1)+top(ref,3))     && (pos_y(i)>=top(ref,2)   && pos_y(i)<=top(ref,2)+top(ref,4));
+%         face1_binary(i)=(pos_x(i)>=face1(ref,1) && pos_x(i) <= face1(ref,1) + face1(ref,3)) && (pos_y(i)>= face1(ref,2) && pos_y(i) <= face1(ref,2)+face1(ref,4));
+%         face2_binary(i)=(pos_x(i)>=face2(ref,1) && pos_x(i) <= face2(ref,1) + face2(ref,3)) && (pos_y(i)>= face2(ref,2) && pos_y(i) <= face2(ref,2)+face2(ref,4));
+%         face3_binary(i)=(pos_x(i)>=face3(ref,1) && pos_x(i) <= face3(ref,1) + face3(ref,3)) && (pos_y(i)>= face3(ref,2) && pos_y(i) <= face3(ref,2)+face3(ref,4));
+%     end
+% end
+
 %% Gaze RLE
 look_duration = 6;                              % let a look be at least 6 frames
-for look_duration = 3:20
+% for look_duration = 3:20
     frame_gaze = gaze_RLE(frame_binary, look_duration);
     shark_gaze = gaze_RLE(shark_binary, look_duration);
     top_gaze   = gaze_RLE(top_binary, look_duration);
@@ -154,9 +153,9 @@ for look_duration = 3:20
     face2_gaze = gaze_RLE(face2_binary, look_duration);
     face3_gaze = gaze_RLE(face3_binary, look_duration);
 
-    name = sprintf('vid002_test_gaze_%2.0f-%d.mat',conf*100,look_duration);
+    name = sprintf('%s_test_gaze_%2.0f-%d.mat',foldername,conf*100,look_duration);
     save(name,'frame_gaze','shark_gaze','top_gaze','face1_gaze','face2_gaze','face3_gaze');
-end
+% end
 
 
         
--- Confusion Matrix/updated_gaze_metric.m
+++ Confusion Matrix/updated_gaze_metric.m
@@ -1,4 +1,4 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % this is the updated gaze metric script
 % Need:
 % - gt gaze (not binary)
@@ -7,13 +7,14 @@
 
 clear all
 close all
-addpath('Vid002/25Conf');
+foldername = 'vid004';
+addpath(foldername);
 
 index = 1;
 look_duration = 6;
 % for look_duration = 3:20
-    gt_name = sprintf('vid002_gt_gaze_25-%d.mat', look_duration);
-    test_name = sprintf('vid002_test_gaze_25-%d.mat', look_duration);
+    gt_name = sprintf('%s_gt_gaze_25-%d.mat', foldername, look_duration);
+    test_name = sprintf('%s_test_gaze_25-%d.mat', foldername, look_duration);
     load(gt_name);
     load(test_name);
 
@@ -34,7 +35,7 @@ look_duration = 6;
     %% Calc metric set #3
     [frame_accuracy, frame_fp, frame_fn] = gaze_metric_3(frame_overlap, frame_union, frame_test_binary, frame_gt_binary);
     [shark_accuracy, shark_fp, shark_fn] = gaze_metric_3(shark_overlap, shark_union, shark_test_binary, shark_gt_binary);
-    [top_accuracy, top_fp, top_fn] = gaze_metric_3(top_overlap, top_union, top_test_binary, top_gt_binary);
+    [top_accuracy, top_fp, top_fn]       = gaze_metric_3(top_overlap, top_union, top_test_binary, top_gt_binary);
     [face1_accuracy, face1_fp, face1_fn] = gaze_metric_3(face1_overlap, face1_union, face1_test_binary, face1_gt_binary);
     [face2_accuracy, face2_fp, face2_fn] = gaze_metric_3(face2_overlap, face2_union, face2_test_binary, face2_gt_binary);
     [face3_accuracy, face3_fp, face3_fn] = gaze_metric_3(face3_overlap, face3_union, face3_test_binary, face3_gt_binary);
@@ -42,40 +43,130 @@ look_duration = 6;
     %% Save values
     total_frame_accuracy(index) = frame_accuracy;
     total_shark_accuracy(index) = shark_accuracy;
-    total_top_accuracy(index) = top_accuracy;
+    total_top_accuracy(index)   = top_accuracy;
     total_face1_accuracy(index) = face1_accuracy;
     total_face2_accuracy(index) = face2_accuracy;
     total_face3_accuracy(index) = face3_accuracy;
     index = index + 1;
+    
 % end
 
+%% Group results
+final_frame = [frame_accuracy, frame_fp, frame_fn];
+final_shark = [shark_accuracy, shark_fp, shark_fn];
+final_top = [top_accuracy, top_fp, top_fn];
+final_face1 = [face1_accuracy, face1_fp, face1_fn];
+final_face2 = [face2_accuracy, face2_fp, face2_fn];
+final_face3 = [face3_accuracy, face3_fp, face3_fn];
+name = sprintf('%s_results',foldername);
+% save(name,'final_frame','final_shark','final_top','final_face1','final_face2','final_face3');
+
+%% Get total vid results
+% dimensions have to match to use | or &
+total_size = max([length(frame_overlap),length(top_overlap),length(shark_overlap), ...
+                    length(face1_overlap),length(face2_overlap),length(face3_overlap)]);
+
+% combine all gt binary vectors
+vid_gt = [frame_gt_binary; zeros(total_size-length(frame_gt_binary),1)] | [top_gt_binary; zeros(total_size-length(top_gt_binary),1)] |...
+         [shark_gt_binary; zeros(total_size-length(shark_gt_binary),1)] | [face1_gt_binary; zeros(total_size-length(face1_gt_binary),1)] |...
+         [face2_gt_binary; zeros(total_size-length(face2_gt_binary),1)] | [face3_gt_binary; zeros(total_size-length(face3_gt_binary),1)];
+     
+% combine all test binary vectors
+vid_test = [frame_test_binary; zeros(total_size-length(frame_test_binary),1)] | [top_test_binary; zeros(total_size-length(top_test_binary),1)] |...
+           [shark_test_binary; zeros(total_size-length(shark_test_binary),1)] | [face1_test_binary; zeros(total_size-length(face1_test_binary),1)] |...
+           [face2_test_binary; zeros(total_size-length(face2_test_binary),1)] | [face3_test_binary; zeros(total_size-length(face3_test_binary),1)];
+     
+% combine all overlaps
+vid_overlap = [frame_overlap; zeros(total_size-length(frame_overlap),1)] | [top_overlap; zeros(total_size-length(top_overlap),1)] |...
+              [shark_overlap; zeros(total_size-length(shark_overlap),1)] | [face1_overlap; zeros(total_size-length(face1_overlap),1)] |...
+              [face2_overlap; zeros(total_size-length(face2_overlap),1)] | [face3_overlap; zeros(total_size-length(face3_overlap),1)];
+
+% combine all unions
+vid_union = [frame_union; zeros(total_size-length(frame_union),1)] | [top_union; zeros(total_size-length(top_union),1)] |...
+            [shark_union; zeros(total_size-length(shark_union),1)] | [face1_union; zeros(total_size-length(face1_union),1)] |...
+            [face2_union; zeros(total_size-length(face2_union),1)] | [face3_union; zeros(total_size-length(face3_union),1)];
+
+vid_acc = sum(vid_overlap)/sum(vid_union);
+vid_fp = sum(vid_test & ~vid_gt)/sum(vid_test);
+vid_fn = sum(vid_gt & ~vid_test)/sum(vid_gt);
+
+vid_overlap2 = [frame_overlap; top_overlap; shark_overlap; face1_overlap; face2_overlap; face3_overlap];
+vid_union2 = [frame_union; top_union; shark_union; face1_union; face2_union; face3_union];
+vid_acc2= sum(vid_overlap2)/sum(vid_union2);
+
+%% Jack's way
+overall_overlap = zeros(7763,1);
+overall_union = zeros(7763,1);
+test = zeros(7763,1);
+for i=1:7763
+    if face1_overlap(i)==1
+        overall_overlap(i)=overall_overlap(i)+1;
+    end
+    if face2_overlap(i)==1
+        overall_overlap(i)=overall_overlap(i)+1;
+    end
+    if face3_overlap(i)==1
+        overall_overlap(i)=overall_overlap(i)+1;
+    end
+    if frame_overlap(i)==1
+        overall_overlap(i)=overall_overlap(i)+1;
+    end
+    if shark_overlap(i)==1
+        overall_overlap(i)=overall_overlap(i)+1;
+    end
+    if top_overlap(i)==1
+        overall_overlap(i)=overall_overlap(i)+1;
+    end
+    
+   if face1_union(i)==1
+        overall_union(i)=overall_union(i)+1;
+    end
+    if face2_union(i)==1
+        overall_union(i)=overall_union(i)+1;
+    end
+    if face3_union(i)==1
+        overall_union(i)=overall_union(i)+1;
+    end
+    if frame_union(i)==1
+        overall_union(i)=overall_union(i)+1;
+    end
+    if shark_union(i)==1
+        overall_union(i)=overall_union(i)+1;
+    end
+    if top_union(i)==1
+        overall_union(i)=overall_union(i)+1;
+    end 
+end
+
+vid_acc3 = sum(overall_overlap)/sum(overall_union);
+
 %% Graph
-figure
-plot(3:20, total_frame_accuracy);
-title('Frame accuracy vs. look duration');
-xlabel('Look duration'); ylabel('Accuracy');
-
-figure
-plot(3:20, total_shark_accuracy);
-title('Shark accuracy vs. look duration');
-xlabel('Look duration'); ylabel('Accuracy');
-
-figure
-plot(3:20, total_top_accuracy);
-title('Top accuracy vs. look duration');
-xlabel('Look duration'); ylabel('Accuracy');
-
-figure
-plot(3:20, total_face1_accuracy);
-title('Face 1 accuracy vs. look duration');
-xlabel('Look duration'); ylabel('Accuracy');
-
-figure
-plot(3:20, total_face2_accuracy);
-title('Face 2 accuracy vs. look duration');
-xlabel('Look duration'); ylabel('Accuracy');
-
-figure
-plot(3:20, total_face3_accuracy);
-title('Face 3 accuracy vs. look duration');
-xlabel('Look duration'); ylabel('Accuracy');
+% figure
+% plot(3:20, total_frame_accuracy);
+% title('Frame accuracy vs. look duration');
+% xlabel('Look duration'); ylabel('Accuracy');
+% 
+% figure
+% plot(3:20, total_shark_accuracy);
+% title('Shark accuracy vs. look duration');
+% xlabel('Look duration'); ylabel('Accuracy');
+% 
+% figure
+% plot(3:20, total_top_accuracy);
+% title('Top accuracy vs. look duration');
+% xlabel('Look duration'); ylabel('Accuracy');
+% 
+% figure
+% plot(3:20, total_face1_accuracy);
+% title('Face 1 accuracy vs. look duration');
+% xlabel('Look duration'); ylabel('Accuracy');
+% 
+% figure
+% plot(3:20, total_face2_accuracy);
+% title('Face 2 accuracy vs. look duration');
+% xlabel('Look duration'); ylabel('Accuracy');
+% 
+% figure
+% plot(3:20, total_face3_accuracy);
+% title('Face 3 accuracy vs. look duration');
+% xlabel('Look duration'); ylabel('Accuracy');
