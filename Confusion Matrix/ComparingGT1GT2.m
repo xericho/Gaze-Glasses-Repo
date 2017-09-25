0dabc42b5cddf860aec350550ff423dde1e8e77a
xericho <xeric.hox@gmail.com>
2017-09-25 13:57:16 -0700

    now it uses all vids

--- Confusion Matrix/ComparingGT1GT2.m
+++ Confusion Matrix/ComparingGT1GT2.m
@@ -9,68 +9,80 @@ clear all
 close all
 restoredefaultpath
 
-foldername = 'vid004';
+foldername = 'vid002';
 addpath(foldername);
 addpath(sprintf('%s/Results',foldername));
+
+RESULTS = [];
 
 %% What do you want to compare?
 % Choose only ONE to be true
 
 GT1 = false;             % GT-1 vs. alg
-GT2 = true;            % GT-2 gt vs. alg
-compare_gts = false;    % GT-1 vs. GT-2
+GT2 = false;            % GT-2 gt vs. alg
+compare_gts = true;    % GT-1 vs. GT-2
 
 % Save the results?
 save_data = true;
 
-%% Get total length of video
-reader = VideoReader(sprintf('%s_raw_60fps.mp4',foldername));
-vid_length = reader.NumberOfFrames;
-
-RESULTS = [];
-
 %% Get accuracy for GT-1 (student) vs Alg
 if(GT1)
-    for exit = 5:20
-        for entry = 5:20
-            % load student gt
-            gt_name = sprintf('%s_gt_gaze_%d-%d.mat', foldername, entry, exit);
-            load(gt_name);
-            % load alg data
-            test_name = sprintf('%s_test_gaze_%d-%d.mat', foldername, entry, exit);
-            load(test_name);
+        for exit = 5:20
+            for entry = 3:20
+                % initialize variables
+                frame_gt_binary = []; frame_test_binary = []; frame_overlap = []; frame_union = [];
+                shark_gt_binary = []; shark_test_binary = []; shark_overlap = []; shark_union = [];
+                top_gt_binary = []; top_test_binary = []; top_overlap = []; top_union = []; 
+                face1_gt_binary = []; face1_test_binary = []; face1_overlap = []; face1_union = []; 
+                face2_gt_binary = []; face2_test_binary = []; face2_overlap = []; face2_union = []; 
+                face3_gt_binary = []; face3_test_binary = []; face3_overlap = []; face3_union = [];
+               
+                for vid = 0:4
+                    restoredefaultpath
+                    foldername = sprintf('vid00%d',i);
+                    addpath(foldername);
+                    addpath(sprintf('%s/Results',foldername));
 
-            % Convert gaze to a vector
-            [frame_gt_binary, frame_test_binary, frame_overlap, frame_union] = gaze2vecNOGRAPH(frame_gt_gaze, frame_gaze, vid_length);
-            [shark_gt_binary, shark_test_binary, shark_overlap, shark_union] = gaze2vecNOGRAPH(shark_gt_gaze, shark_gaze, vid_length);
-            [top_gt_binary, top_test_binary, top_overlap, top_union] = gaze2vecNOGRAPH(top_gt_gaze, top_gaze, vid_length);
-            [face1_gt_binary, face1_test_binary, face1_overlap, face1_union] = gaze2vecNOGRAPH(face1_gt_gaze, face1_gaze, vid_length);
-            [face2_gt_binary, face2_test_binary, face2_overlap, face2_union] = gaze2vecNOGRAPH(face2_gt_gaze, face2_gaze, vid_length);
-            [face3_gt_binary, face3_test_binary, face3_overlap, face3_union] = gaze2vecNOGRAPH(face3_gt_gaze, face3_gaze, vid_length);
+                    % Get total length of video
+                    reader = VideoReader(sprintf('%s_raw_60fps.mp4',foldername));
+                    vid_length = reader.NumberOfFrames;
+        
+                    % load student gt
+                    gt_name = sprintf('%s_gt_gaze_%d-%d.mat', foldername, entry, exit);
+                    load(gt_name);
+                    % load alg data
+                    test_name = sprintf('%s_test_gaze_%d-%d.mat', foldername, entry, exit);
+                    load(test_name);
 
-            % Calc metric set #3
-            [frame_accuracy, frame_fp, frame_fn] = gaze_metric_3(frame_overlap, frame_union, frame_test_binary, frame_gt_binary);
-            [shark_accuracy, shark_fp, shark_fn] = gaze_metric_3(shark_overlap, shark_union, shark_test_binary, shark_gt_binary);
-            [top_accuracy, top_fp, top_fn]       = gaze_metric_3(top_overlap, top_union, top_test_binary, top_gt_binary);
-            [face1_accuracy, face1_fp, face1_fn] = gaze_metric_3(face1_overlap, face1_union, face1_test_binary, face1_gt_binary);
-            [face2_accuracy, face2_fp, face2_fn] = gaze_metric_3(face2_overlap, face2_union, face2_test_binary, face2_gt_binary);
-            [face3_accuracy, face3_fp, face3_fn] = gaze_metric_3(face3_overlap, face3_union, face3_test_binary, face3_gt_binary);
+                    % Convert gaze to a vector
+                    [frame_gt_binary, frame_test_binary, frame_overlap, frame_union] = gaze2vec_ALL(frame_gt_gaze, frame_gaze, frame_gt_binary, frame_test_binary, frame_overlap, frame_union);
+                    [shark_gt_binary, shark_test_binary, shark_overlap, shark_union] = gaze2vec_ALL(shark_gt_gaze, shark_gaze, shark_gt_binary, shark_test_binary, shark_overlap, shark_union);
+                    [top_gt_binary, top_test_binary, top_overlap, top_union] = gaze2vec_ALL(top_gt_gaze, top_gaze, top_gt_binary, top_test_binary, top_overlap, top_union);
+                    [face1_gt_binary, face1_test_binary, face1_overlap, face1_union] = gaze2vec_ALL(face1_gt_gaze, face1_gaze, face1_gt_binary, face1_test_binary, face1_overlap, face1_union);
+                    [face2_gt_binary, face2_test_binary, face2_overlap, face2_union] = gaze2vec_ALL(face2_gt_gaze, face2_gaze, face2_gt_binary, face2_test_binary, face2_overlap, face2_union);
+                    [face3_gt_binary, face3_test_binary, face3_overlap, face3_union] = gaze2vec_ALL(face3_gt_gaze, face3_gaze, face3_gt_binary, face3_test_binary, face3_overlap, face3_union);
 
-            % Calc total vid results
-            vid_overlap = [frame_overlap; top_overlap; shark_overlap; face1_overlap; face2_overlap; face3_overlap];
-            vid_union = [frame_union; top_union; shark_union; face1_union; face2_union; face3_union];
-            vid_acc= sum(vid_overlap)/sum(vid_union);
+                    % Calc metric set #3
+                    [frame_accuracy, frame_fp, frame_fn] = gaze_metric_3(frame_overlap, frame_union, frame_test_binary, frame_gt_binary);
+                    [shark_accuracy, shark_fp, shark_fn] = gaze_metric_3(shark_overlap, shark_union, shark_test_binary, shark_gt_binary);
+                    [top_accuracy, top_fp, top_fn]       = gaze_metric_3(top_overlap, top_union, top_test_binary, top_gt_binary);
+                    [face1_accuracy, face1_fp, face1_fn] = gaze_metric_3(face1_overlap, face1_union, face1_test_binary, face1_gt_binary);
+                    [face2_accuracy, face2_fp, face2_fn] = gaze_metric_3(face2_overlap, face2_union, face2_test_binary, face2_gt_binary);
+                    [face3_accuracy, face3_fp, face3_fn] = gaze_metric_3(face3_overlap, face3_union, face3_test_binary, face3_gt_binary);
+                end
+                % Calc total vid results
+                vid_overlap = [frame_overlap; top_overlap; shark_overlap; face1_overlap; face2_overlap; face3_overlap];
+                vid_union = [frame_union; top_union; shark_union; face1_union; face2_union; face3_union];
+                vid_acc = sum(vid_overlap)/sum(vid_union);
 
-            vid_test = [frame_test_binary; top_test_binary; shark_test_binary; face1_test_binary; face2_test_binary; face3_test_binary;];
-            vid_gt = [frame_gt_binary; top_gt_binary; shark_gt_binary; face1_gt_binary; face2_gt_binary; face3_gt_binary;];
-            vid_fp = sum(vid_test & ~vid_gt)/sum(vid_test);
-            vid_fn = sum(vid_gt & ~vid_test)/sum(vid_gt);
-
-            RESULTS(exit,entry) = vid_acc;
+                RESULTS(exit,entry) = vid_acc;
+            end
         end
-    end
+
+    % find what max is and where
     max_value = max(RESULTS(RESULTS>0));
     [row, col] = find(RESULTS == max(RESULTS(RESULTS>0)));
+    
     if(save_data)
         % Saving results
         name = sprintf('%s_studentVSalg_results.mat',foldername); 
@@ -79,10 +91,13 @@ if(GT1)
 end
 %% Get accuracy for Alg vs GT-2 (Leanne)
 if(GT2)
+    % Get total length of video
+    reader = VideoReader(sprintf('%s_raw_60fps.mp4',foldername));
+    vid_length = reader.NumberOfFrames;
     % load leanne gt
     load(sprintf('%s_leanne_gt_gaze.mat',foldername));
     for exit = 5:20
-        for entry = 5:20
+        for entry = 3:20
             
             test_name = sprintf('%s_test_gaze_%d-%d.mat', foldername, entry, exit);
             load(test_name);
@@ -95,25 +110,14 @@ if(GT2)
             [face2_gt_binary2, face2_test_binary2, face2_overlap2, face2_union2] = gaze2vecNOGRAPH(face2_leanne_gt_gaze, face2_gaze, vid_length);
             [face3_gt_binary2, face3_test_binary2, face3_overlap2, face3_union2] = gaze2vecNOGRAPH(face3_leanne_gt_gaze, face3_gaze, vid_length);
 
-            % Calc metric set #3
-            [frame_accuracy2, frame_fp2, frame_fn2] = gaze_metric_3(frame_overlap2, frame_union2, frame_test_binary2, frame_gt_binary2);
-            [shark_accuracy2, shark_fp2, shark_fn2] = gaze_metric_3(shark_overlap2, shark_union2, shark_test_binary2, shark_gt_binary2);
-            [top_accuracy2, top_fp2, top_fn2]       = gaze_metric_3(top_overlap2, top_union2, top_test_binary2, top_gt_binary2);
-            [face1_accuracy2, face1_fp2, face1_fn2] = gaze_metric_3(face1_overlap2, face1_union2, face1_test_binary2, face1_gt_binary2);
-            [face2_accuracy2, face2_fp2, face2_fn2] = gaze_metric_3(face2_overlap2, face2_union2, face2_test_binary2, face2_gt_binary2);
-            [face3_accuracy2, face3_fp2, face3_fn2] = gaze_metric_3(face3_overlap2, face3_union2, face3_test_binary2, face3_gt_binary2);
-
             % Calc total vid results
             vid_overlap2 = [frame_overlap2; top_overlap2; shark_overlap2; face1_overlap2; face2_overlap2; face3_overlap2];
             vid_union2 = [frame_union2; top_union2; shark_union2; face1_union2; face2_union2; face3_union2];
-            vid_acc2 = sum(vid_overlap2)/sum(vid_union2);
-
             vid_test2 = [frame_test_binary2; top_test_binary2; shark_test_binary2; face1_test_binary2; face2_test_binary2; face3_test_binary2;];
             vid_gt2 = [frame_gt_binary2; top_gt_binary2; shark_gt_binary2; face1_gt_binary2; face2_gt_binary2; face3_gt_binary2;];
-            vid_fp2 = sum(vid_test2 & ~vid_gt2)/sum(vid_test2);
-            vid_fn2 = sum(vid_gt2 & ~vid_test2)/sum(vid_gt2);
 
-            RESULTS(exit,entry) = vid_acc2;
+            [vid_acc, vid_fp, vid_fn] = gaze_metric_3(vid_overlap2, vid_union2, vid_test2, vid_gt2);
+            RESULTS(exit,entry) = vid_acc;
         end
     end
     max_value = max(RESULTS(RESULTS>0));
@@ -124,50 +128,60 @@ if(GT2)
         save(name,'RESULTS','max_value','row','col');
     end
 end
+
 %% Get accuracy for GT-1 (student) vs GT-2 (Leanne)
 if(compare_gts)
-    % load leanne gt
-    load(sprintf('%s_leanne_gt_gaze.mat',foldername));
-    for exit = 5:20
-        for entry = 5:20
-            % load student gt
-            gt_name = sprintf('%s_gt_gaze_%d-%d.mat', foldername, entry, exit);
-            load(gt_name);
+        for exit = 5:20
+            for entry = 3:20
+                % initialize variables
+                frame_gt_binary = []; frame_test_binary = []; frame_overlap = []; frame_union = [];
+                shark_gt_binary = []; shark_test_binary = []; shark_overlap = []; shark_union = [];
+                top_gt_binary = []; top_test_binary = []; top_overlap = []; top_union = []; 
+                face1_gt_binary = []; face1_test_binary = []; face1_overlap = []; face1_union = []; 
+                face2_gt_binary = []; face2_test_binary = []; face2_overlap = []; face2_union = []; 
+                face3_gt_binary = []; face3_test_binary = []; face3_overlap = []; face3_union = [];
+               
+                for vid = 0:3
+                    restoredefaultpath
+                    foldername = sprintf('vid00%d', vid);
+                    addpath(foldername);
+                    addpath(sprintf('%s/Results',foldername));
 
-            % Convert gaze to a vector
-            [frame_gt_binary, frame_test_binary, frame_overlap, frame_union] = gaze2vecNOGRAPH(frame_gt_gaze, frame_leanne_gt_gaze, vid_length);
-            [shark_gt_binary, shark_test_binary, shark_overlap, shark_union] = gaze2vecNOGRAPH(shark_gt_gaze, shark_leanne_gt_gaze, vid_length);
-            [top_gt_binary, top_test_binary, top_overlap, top_union] = gaze2vecNOGRAPH(top_gt_gaze, top_leanne_gt_gaze, vid_length);
-            [face1_gt_binary, face1_test_binary, face1_overlap, face1_union] = gaze2vecNOGRAPH(face1_gt_gaze, face1_leanne_gt_gaze, vid_length);
-            [face2_gt_binary, face2_test_binary, face2_overlap, face2_union] = gaze2vecNOGRAPH(face2_gt_gaze, face2_leanne_gt_gaze, vid_length);
-            [face3_gt_binary, face3_test_binary, face3_overlap, face3_union] = gaze2vecNOGRAPH(face3_gt_gaze, face3_leanne_gt_gaze, vid_length);
+                    % Get total length of video
+                    reader = VideoReader(sprintf('%s_raw_60fps.mp4',foldername));
+                    vid_length = reader.NumberOfFrames;
+        
+                    % load student gt
+                    gt_name = sprintf('%s_gt_gaze_%d-%d.mat', foldername, entry, exit);
+                    load(gt_name);
+                    % load leanne gt
+                    load(sprintf('%s_leanne_gt_gaze.mat',foldername));
 
-            % Calc metric set #3
-            [frame_accuracy, frame_fp, frame_fn] = gaze_metric_3(frame_overlap, frame_union, frame_test_binary, frame_gt_binary);
-            [shark_accuracy, shark_fp, shark_fn] = gaze_metric_3(shark_overlap, shark_union, shark_test_binary, shark_gt_binary);
-            [top_accuracy, top_fp, top_fn]       = gaze_metric_3(top_overlap, top_union, top_test_binary, top_gt_binary);
-            [face1_accuracy, face1_fp, face1_fn] = gaze_metric_3(face1_overlap, face1_union, face1_test_binary, face1_gt_binary);
-            [face2_accuracy, face2_fp, face2_fn] = gaze_metric_3(face2_overlap, face2_union, face2_test_binary, face2_gt_binary);
-            [face3_accuracy, face3_fp, face3_fn] = gaze_metric_3(face3_overlap, face3_union, face3_test_binary, face3_gt_binary);
+                    % Convert gaze to a vector
+                    [frame_gt_binary, frame_test_binary, frame_overlap, frame_union] = gaze2vec_ALL(frame_gt_gaze, frame_leanne_gt_gaze, frame_gt_binary, frame_test_binary, frame_overlap, frame_union);
+                    [shark_gt_binary, shark_test_binary, shark_overlap, shark_union] = gaze2vec_ALL(shark_gt_gaze, shark_leanne_gt_gaze, shark_gt_binary, shark_test_binary, shark_overlap, shark_union);
+                    [top_gt_binary, top_test_binary, top_overlap, top_union] = gaze2vec_ALL(top_gt_gaze, top_leanne_gt_gaze, top_gt_binary, top_test_binary, top_overlap, top_union);
+                    [face1_gt_binary, face1_test_binary, face1_overlap, face1_union] = gaze2vec_ALL(face1_gt_gaze, face1_leanne_gt_gaze, face1_gt_binary, face1_test_binary, face1_overlap, face1_union);
+                    [face2_gt_binary, face2_test_binary, face2_overlap, face2_union] = gaze2vec_ALL(face2_gt_gaze, face2_leanne_gt_gaze, face2_gt_binary, face2_test_binary, face2_overlap, face2_union);
+                    [face3_gt_binary, face3_test_binary, face3_overlap, face3_union] = gaze2vec_ALL(face3_gt_gaze, face3_leanne_gt_gaze, face3_gt_binary, face3_test_binary, face3_overlap, face3_union);
+                end
+                
+                % Calc acc for all vids
+                vid_overlap = [frame_overlap; top_overlap; shark_overlap; face1_overlap; face2_overlap; face3_overlap];
+                vid_union = [frame_union; top_union; shark_union; face1_union; face2_union; face3_union];
+                vid_acc = sum(vid_overlap)/sum(vid_union);
 
-            % Calc total vid results
-            vid_overlap = [frame_overlap; top_overlap; shark_overlap; face1_overlap; face2_overlap; face3_overlap];
-            vid_union = [frame_union; top_union; shark_union; face1_union; face2_union; face3_union];
-            vid_acc= sum(vid_overlap)/sum(vid_union);
-
-            vid_test = [frame_test_binary; top_test_binary; shark_test_binary; face1_test_binary; face2_test_binary; face3_test_binary;];
-            vid_gt = [frame_gt_binary; top_gt_binary; shark_gt_binary; face1_gt_binary; face2_gt_binary; face3_gt_binary;];
-            vid_fp = sum(vid_test & ~vid_gt)/sum(vid_test);
-            vid_fn = sum(vid_gt & ~vid_test)/sum(vid_gt);
-
-            RESULTS(exit,entry) = vid_acc;
+                RESULTS(exit,entry) = vid_acc;
+            end
         end
-    end
+
+    % find what max is and where
     max_value = max(RESULTS(RESULTS>0));
     [row, col] = find(RESULTS == max(RESULTS(RESULTS>0)));
+    
     if(save_data)
         % Saving results
-        name = sprintf('%s_compare_gt_results.mat',foldername); 
+        name = sprintf('%s_compare_gt_results.mat',foldername);  
         save(name,'RESULTS','max_value','row','col');
     end
 end
@@ -188,8 +202,7 @@ end
 xlabel('Entry');
 ylabel('Exit');
 % shift to the matrix we want
-xlim([5 20]);
-% set(gca,'ydir','reverse')
+xlim([3 20]);
 ylim([5 20]);
 % mark each tick
-set(gca, 'YDir', 'normal', 'XTick', 5:20, 'YTick', 5:20);
+set(gca, 'YDir', 'normal', 'XTick', 3:20, 'YTick', 5:20);
